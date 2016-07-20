52b63a977d3820ac60c03e2bb09e85fb8b06c5da
Rémi Verschelde <rverschelde@gmail.com>
2016-07-20 23:39:20 +0200

    conf.py: Prevent RTD from overriding our theme config
    Also tweak the logo a bit.

--- conf.py
+++ conf.py
@@ -49,10 +49,11 @@ highlight_language = 'gdscript'
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if on_rtd:
+	using_rtd_theme = True
 
 # Theme options
 html_theme_options = {
