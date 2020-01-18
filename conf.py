72e37935e73b629fd9ecaabf958943cabc3ac825
Nathan Lovato <nathan@gdquest.com>
2020-01-18 19:30:20 +0100

    Add robots.txt file to prevent indexing outdated docs (#3002)
    Closes #2912

--- conf.py
+++ conf.py
@@ -91,6 +91,8 @@ html_logo = 'img/docs_logo.png'
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
 
+html_extra_path = ['robots.txt']
+
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
