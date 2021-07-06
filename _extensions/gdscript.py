60265b4c458e4b80b4164f564251e2f30e91ab63
Max Hilbrunner <m.hilbrunner@gmail.com>
2021-07-06 07:00:10 +0200

    Fix Sphinx warning about GDScriptLexer
    (cherry picked from commit ca680690246a36caf37a1afb4d6fa4305b4084df)

--- _extensions/gdscript.py
+++ _extensions/gdscript.py
@@ -339,7 +339,7 @@ class GDScriptLexer(RegexLexer):
 
 
 def setup(sphinx):
-    sphinx.add_lexer("gdscript", GDScriptLexer())
+    sphinx.add_lexer("gdscript", GDScriptLexer)
 
     return {
         "parallel_read_safe": True,
