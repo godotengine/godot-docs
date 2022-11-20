4096508fb6114e0e596350afd27d7cb739f21ad8
Yuri Sizov <11782833+YuriSizov@users.noreply.github.com>
2022-11-20 14:10:09 +0300

    Change the square logo for a wide one (#6397)
    Co-authored-by: Patchcoat <spiencewelch@gmail.com>

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -16,13 +16,13 @@ const registerOnScrollEvent = (function(){
   // Configuration.
 
   // The number of pixels the user must scroll by before the logo is completely hidden.
-  const scrollTopPixels = 234;
+  const scrollTopPixels = 84;
   // The target margin to be applied to the navigation bar when the logo is hidden.
-  const menuTopMargin = 90;
+  const menuTopMargin = 88;
   // The max-height offset when the logo is completely visible.
-  const menuHeightOffset_default = 338;
+  const menuHeightOffset_default = 180;
   // The max-height offset when the logo is completely hidden.
-  const menuHeightOffset_fixed = 102;
+  const menuHeightOffset_fixed = 98;
   // The distance between the two max-height offset values above; used for intermediate values.
   const menuHeightOffset_diff = (menuHeightOffset_default - menuHeightOffset_fixed);
 
