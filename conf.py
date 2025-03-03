6fd5aa26e3cd55db382630c591148730f85115c9
Rémi Verschelde <rverschelde@gmail.com>
2025-03-03 17:34:13 +0100

    Fix version for Edit on GitHub, should link to master

--- conf.py
+++ conf.py
@@ -184,7 +184,7 @@ html_context = {
     "display_github": not is_i18n,  # Integrate GitHub
     "github_user": "godotengine",  # Username
     "github_repo": "godot-docs",  # Repo name
-    "github_version": "4.4",  # Version
+    "github_version": "master",  # Version for Edit on GitHub - keep `master` for 4.x branches
     "conf_py_path": "/",  # Path in the checkout to the docs root
     "godot_docs_title": supported_languages[language],
     "godot_docs_basepath": "https://docs.godotengine.org/",
