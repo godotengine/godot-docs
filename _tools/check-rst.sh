7910b2c622e3f476e308a2066a5720faa212e910
Danil Alexeev <dalexeev12@yandex.ru>
2026-04-01 13:53:04 +0300

    Replace remaining links to the contributing section

--- _tools/check-rst.sh
+++ _tools/check-rst.sh
@@ -2,7 +2,7 @@
 
 set -uo pipefail
 
-output=$(grep -r -P '^(?!\s*\.\.).*\S::$' --include='*.rst' --exclude='docs_writing_guidelines.rst' .)
+output=$(grep -r -P '^(?!\s*\.\.).*\S::$' --include='*.rst' .)
 if [[ -n $output ]]; then
   echo 'The shorthand codeblock syntax (trailing `::`) is not allowed.'
   echo "$output"
